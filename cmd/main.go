bd041022156ff75b058f3fff63fe1aab3339d85e
Micael Rocha <69124656+eu-micaeu@users.noreply.github.com>
2024-01-10 17:29:01 -0300

    Atualizando o site.

---
+++ cmd/main.go
@@ -0,0 +1,34 @@
+package main
+
+import (
+	"net/http"
+
+	"github.com/eu-micaeu/IsaSemiJoias-WebSite/middlewares"
+	"github.com/gin-gonic/gin"
+	
+)
+
+func main() {
+
+	r := gin.Default()
+
+	r.Use(middlewares.CorsMiddleware())
+
+	r.LoadHTMLGlob("./views/*.html")
+
+	r.GET("/index", func(c *gin.Context) {
+
+		c.HTML(http.StatusOK, "index.html", nil)
+
+	})
+
+	r.GET("/semijoia", func(c *gin.Context) {
+
+		c.HTML(http.StatusOK, "semijoia.html", nil)
+
+	})
+
+	r.Static("./static", "./static")
+
+	r.Run()
+}
---
+++ static/js/index.js
@@ -0,0 +1,6 @@
+document.addEventListener("DOMContentLoaded", () => {
+    const joia1 = document.getElementById("joia1");
+    joia1.addEventListener("click", () => {
+        window.location.href = "/semijoia";
+    });
+});
